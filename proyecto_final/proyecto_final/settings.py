22a4e872aa1e392553e030a40282a02eeca006a9
gustavo <glsaaved@gmail.com>
2015-11-16 16:57:29 -0300

    Actualizacion : settings : hora y lenguaje

--- proyecto_final/proyecto_final/settings.py
+++ proyecto_final/proyecto_final/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
